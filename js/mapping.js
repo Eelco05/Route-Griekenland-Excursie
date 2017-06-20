1abfd2a8be2c11a2789ed6d2cdad455a47edd880
Eelco05 <eelco.alink@gmail.com>
2017-06-20 20:55:33 +0200

    dag 4

--- js/mapping.js
+++ js/mapping.js
@@ -1,6 +1,6 @@
 var locations = [
     [
-        'Begintpunt', 
+        'Begintpunt <b>Kerkyra</b>', 
         39.610603, 19.927313,
         '2 meter boven zeeniveau', 
         'Begin van de excursie en eerste uitleg.',
@@ -31,42 +31,42 @@ var locations = [
         'Akropolis ', 
         39.602834, 19.924952, 
         '44 meter boven zeeniveau', 
-        'Akropolis van het Kerkyra van de Oudheid.',
+        'Akropolis van het <b>Kerkyra</b> van de Oudheid.',
         'icons/icon_I.png'
     ],
     [
         'Kardaki Tempel', 
         39.60152154, 19.92597684, 
         '35 meter boven zeeniveau', 
-        'Mogelijk gewijd aan Apollo. Door kusterosie is de helft hiervan in zee gevallen.',
+        'Mogelijk gewijd aan Apollo. <br>Door kusterosie is de helft hiervan in zee gevallen.',
         'icons/icon_I.png'
     ],
     [
-        'Ingang Kasteel Corfu', 
+        'Ingang Kasteel <b>Kerkyra</b>', 
         39.62338969, 19.92593631, 
         '10 meter boven zeeniveau', 
-        'Ingang van het Venitiaans kasteel van Kerkyra.',
+        'Ingang van het Venitiaans kasteel van <b>Kerkyra</b>.',
         'icons/icon_I.png'
     ],
     [
-        'Top Kasteel Corfu', 
+        'Top Kasteel <b>Kerkyra</b>', 
         39.62354877, 19.92884919, 
         '24 meter boven zeeniveau', 
-        'Hoogste deel van het Kasteel. Uitzicht dat zowel de stad als het vaarwater om het eiland beheerst.',
+        'Hoogste deel van het Kasteel. <br>Uitzicht dat zowel de stad als het vaarwater om het eiland beheerst.',
         'icons/icon_I.png'
     ],
     [
         'Overnachting', 
         39.60863496, 19.92473871, 
         '13 meter boven zeeniveau', 
-        'Overnachting in Corfu.',
+        'Overnachting <b>Kerkyra</b>.',
         'icons/hotel.png'
     ],
     [
         'Haven Corfu', 
         39.6300852, 19.90158319, 
         '0 meter boven zeeniveau', 
-        'Vertrek Corfu dag 2.',
+        'Vertrek <b>Kerkyra</b> dag 2.',
         'icons/anchor.png'
     ],
     [
@@ -87,39 +87,39 @@ var locations = [
         'Busreis', 
         39.37650661, 20.4015255, 
         '120 meter boven zeeniveau', 
-        'Onderweg van Igoumenitsa naar Necromanteion.',
+        'Onderweg van <b>Igoumenitsa</b> naar <b>Necromanteion</b>.',
         'icons/bus_right.png'
     ],
     [
         'Necromanteion', 
         39.23610492, 20.5340454, 
         '38 meter boven zeeniveau', 
-        'Mogelijke locatie Necromanteion uit bronnen, maar was net zo waarschijnlijk versterkte boerderij.',
+        'Mogelijke locatie <b>Necromanteion</b> uit bronnen, maar was net zo waarschijnlijk versterkte boerderij.',
         'icons/icon_II.png'
     ],
     [
         'Busreis', 
         39.12686432, 20.59387207, 
         '13 meter boven zeeniveau', 
-        'Onderweg van Necromanteion naar Nicopolis.',
+        'Onderweg van <b>Necromanteion</b> naar <b>Nicopolis</b>.',
         'icons/bus_right.png'
     ],
     [
-        'Nicopolis', 
+        'Nicopolis, stad', 
         39.00899684, 20.73358297, 
         '28 meter boven zeeniveau', 
-        'Romeinse stad, gebouwd ter ere van de slag bij Actium.',
+        'Romeinse stad, gebouwd ter ere van de <b>Slag bij Actium</b>.',
         'icons/icon_II.png'
     ],
     [
-        'Poort Nicopolis', 
+        'Nicopolis, stadspoort ', 
         39.01019733, 20.731287, 
         '20 meter boven zeeniveau', 
-        'Gerestoreerde en gereconstrueerde poort in de ommuring van Nicopolis.',
+        'Gerestoreerde en gereconstrueerde poort in de ommuring van <b>Nicopolis</b>.',
         'icons/icon_II.png'
     ],
     [
-        'Odeon Nicopolis', 
+        'Nicopolis, Odeon', 
         39.01106434, 20.72911978, 
         '23 meter boven zeeniveau', 
         'Kleiner dan een normaal Grieks theater, in dit odeon werden zang-voorstellingen en oraties gehouden.',
@@ -129,42 +129,42 @@ var locations = [
         'Nikopolis Archeologisch Museum', 
         38.9920351, 20.7402533, 
         '20 meter boven zeeniveau', 
-        'Modern museum, waar de archeologische vondsten van Nicopolis en omgeving staan opgesteld.',
+        'Modern museum, waar de archeologische vondsten van <b>Nicopolis</b> en omgeving staan opgesteld.',
         'icons/icon_II.png'
     ],
     [
-        'Overwinningsaltaar bij Nicopolis', 
+        'Nicopolis, overwinningsaltaar', 
         39.02776051, 20.73539615, 
         '76 meter boven zeeniveau', 
-        'Altaar gebouwd om de slag bij Actium in 31 v. Chr. te herdenken. <br>Hierbij versloeg Octavianus in een zeeslag de gezamelijke vloot van Marcus Antonius en Cleopatra, waarna Octavianus Augustus werd, eerste Keizer van het Romeinse Rijk.',
+        'Altaar gebouwd om de <b>Slag bij Actium</b> (<i>31 v. Chr.</i>) te herdenken. <br>Hierbij versloeg Octavianus in een zeeslag de gezamelijke vloot van Marcus Antonius en Cleopatra. <br> Octavianus werd hiermee Augustus, eerste Keizer van het Romeinse Rijk.',
         'icons/icon_II.png'
     ],
     [
         'Cassope', 
         39.14505583, 20.67461729, 
         '555 meter boven zeeniveau', 
-        'Goed bewaarde Griekse stad uit de 4de eeuw v. Chr.. <br>Wordt verlaten na de aanleg van Nicopolis in 31 v.Chr..',
+        'Goed bewaarde Griekse stad uit de 4de eeuw v. Chr.. <br>Wordt verlaten na de aanleg van <b>Nicopolis</b> door de Romeinen.',
         'icons/icon_II.png'
     ],
     [
         'Busreis', 
         39.42346419, 20.70922852, 
         '459 meter boven zeeniveau', 
-        'Onderweg van Cassope naar Ioannina.',
+        'Onderweg van <b>Cassope</b> naar <b>Ioannina</b>.',
         'icons/bus_right.png'
     ],
     [
         'Kasteel Ioannina', 
         39.67074432, 20.86255431, 
         '484 meter boven zeeniveau', 
-        'Fortificaties bij de stad van Ioannina uit de Ottomaanse Periode, <br>met de eerste fase uit de 11e eeuw en de nu zichtbare resten gebouwd door Ali Pasha in 1795.',
+        'Fortificaties bij de stad van <b>Ioannina</b> uit de Ottomaanse Periode, <br>met de eerste fase uit de 11e eeuw en de nu zichtbare resten gebouwd door Ali Pasha in 1795.',
         'icons/icon_II.png'
     ],
     [
         'Overnachting', 
         39.66346015, 20.85050583, 
         '505 meter boven zeeniveau', 
-        'Overnachting in Ioannina.',
+        'Overnachting in <b>Ioannina</b>.',
         'icons/hotel.png'
     ],
     [
@@ -178,59 +178,198 @@ var locations = [
         'Dodona', 
         39.5464645, 20.7882112, 
         '637 meter boven zeeniveau', 
-        'Archeologische site Dodona. Oudste orakel heiligdom van Griekenland. <br>Politieke hart van de Epirische Bond (370-168 v. Chr.).',
+        'Archeologische site <b>Dodona</b>. <br>Oudste orakel heiligdom van Griekenland. <br>Politieke hart van de Epirische Bond (370-168 v. Chr.).',
         'icons/icon_III.png'
     ],
     [
         'Busreis', 
         40.00447583, 21.38214111, 
         '601 meter boven zeeniveau', 
-        'Onderweg van Dodona naar Aiani.',
+        'Onderweg van <b>Dodona</b> naar <b>Aiani</b>.',
         'icons/bus_right.png'
     ],
     [
-        'Necropolis Aiani', 
+        'Aiani, Necropolis', 
         40.18299638, 21.829319, 
         '389 meter boven zeeniveau', 
-        'Het Necropolis van verschillende elite tombes, gelegen bij Aiani.',
+        'Necropolis met verschillende elite tombes.',
         'icons/icon_III.png'
     ],
     [
-        'Akropolis Aiani', 
+        'Aiani, Akropolis', 
         40.17532384, 21.83104634, 
         '451 meter boven zeeniveau', 
-        'Versterkte binnenstad van het oude Aiani. <br>Veel nijverheids gebouwen nabij het centrum.',
+        'Versterkte binnenstad van het oude <b>Aiani</b>. <br>Veel nijverheids gebouwen nabij het centrum.',
         'icons/icon_III.png'
     ],
     [
         'Busreis', 
         39.91394967, 22.18139648, 
         '338 meter boven zeeniveau', 
-        'Onderweg van Aiani naar Larissa.',
+        'Onderweg van <b>Aiani</b> naar <b>Larissa</b>.',
         'icons/bus_right.png'
     ],
     [
         'Diachronisch Museum van Larissa', 
         39.6184015, 22.3938355, 
         '98 meter boven zeeniveau', 
-        'Splinternieuw museum, opgezet om de archeologische vondsten uit de omgeving van Larissa te tentoon te stellen. <br>Nadruk op diachrone ontwikkelingen, vanaf het Paleolithicum tot de Moderne Tijd.',
+        'Splinternieuw museum, opgezet om de archeologische vondsten uit de omgeving van <b>Larissa</b> te tentoon te stellen. <br>Nadruk op diachrone ontwikkelingen, vanaf het Paleolithicum tot de Moderne Tijd.',
         'icons/icon_III.png'
     ],
     [
         'Theater van Larissa', 
         39.64034723, 22.41526365, 
         '81 meter boven zeeniveau', 
-        'Resten van Oud-Griekse voorganger van Larissa. Meest zichtbaar is het Theater',
+        'Resten van Oud-Griekse voorganger van <b>Larissa</b>. Meest zichtbaar is het Theater',
         'icons/icon_III.png'
     ],
     [
         'Overnachting', 
         39.63725309, 22.4189584, 
         '83 meter boven zeeniveau', 
-        'Overnachting in Larissa.',
+        'Overnachting in <b>Larissa</b>.',
         'icons/hotel.png'
     ],
-    
+    [
+        'Busreis', 
+        40.09895284, 22.55544662, 
+        '39 meter boven zeeniveau', 
+        'Onderweg van <b>Larissa</b> naar <b>Platamonas</b>.',
+        'icons/bus_left.png'
+    ],
+    [
+        'Kasteel van Platamonas', 
+        40.006, 22.5983, 
+        '110 meter boven zeeniveau', 
+        'Kasteel uit de Byzantijnse Periode dat tot in de 20ste eeuw in gebruik is gebleven. <br> Bewaakt een belangrijke landroute tussen Noord- en Zuid-Griekenland.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Busreis', 
+        39.8180299, 22.50961304, 
+        '39 meter boven zeeniveau', 
+        'Onderweg van <b>Platamonas</b> naar <b>Dion</b>.',
+        'icons/bus_right.png'
+    ],
+    [
+        'Dion', 
+        40.1754386, 22.49305844, 
+        '3 meter boven zeeniveau', 
+        'Archeologische site <b>Dion</b>. <br>Belangrijk Macedonisch heiligdom, gelegen onder de heilige berg Olympus. <br> Dateert tussen de 4de eeuw voor en de 4de eeuw na Christus.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Dion, Demeter heiligdom', 
+        40.17556976, 22.49357879, 
+        '3 meter boven zeeniveau', 
+        'Oudste heiligdom van Demeter van Griekenland.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Dion, Zeus Hypsistos heiligdom', 
+        40.17584028, 22.4946624, 
+        '3 meter boven zeeniveau', 
+        'Heiligdom gewijd aan Zeus als oppergod.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Dion, Isis heiligdom', 
+        40.17619278, 22.49521494, 
+        '3 meter boven zeeniveau', 
+        'Heiligdom gewijd aan de Egyptische godin Isis. <br>Centrale weg door het heiligdom symboliseerd de Nijl.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Dion, odeon', 
+        40.17618458, 22.49211431, 
+        '12 meter boven zeeniveau', 
+        'Recentelijk restaureerd odeon, met gemengd succes. <br>Discussiepunt in hoe om te gaan met archeologisch erfgoed.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Dion, Christelijke Basiliek', 
+        40.17684038, 22.4903065, 
+        '13 meter boven zeeniveau', 
+        'Daterend uit eind 4e eeuw n. Chr., deze Basiliek heeft door aardbevingen maar een kort leven gehad.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Dion, agora', 
+        40.17687317, 22.49163687, 
+        '13 meter boven zeeniveau', 
+        'Centrale plein van <b>Dion</b>.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Dion, huizencomplex', 
+        40.1780331, 22.49201775, 
+        '7 meter boven zeeniveau', 
+        'Huizen complex van <b>Dion</b>.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Dion, Villa van Dionysos', 
+        40.17857002, 22.4922055, 
+        '6 meter boven zeeniveau', 
+        'Romeimse villa met een prachtig Dionysos mozaiek. <br>.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Dion, Asklepios heiligdom', 
+        40.17394662, 22.49466777, 
+        '9 meter boven zeeniveau', 
+        'Gewijd aan Asklepios, de Griekse god van geneeskunde en genezing. <br>Door de vele bronnen van <b>Dion</b> was dit een plek waar mensen kwamen voor heeling.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Dion, Zeus Olympus heiligdom', 
+        40.17134992, 22.49491453, 
+        '5 meter boven zeeniveau', 
+        'Heiligdom gewijd aan Zeus van Olympus.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Dion, Romeins Theater', 
+        40.17068792, 22.49549121, 
+        '6 meter boven zeeniveau', 
+        'Theater uit de 2e eeuw na Christus.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Dion, Archeologisch Museum', 
+        40.17052191, 22.48710662, 
+        '27 meter boven zeeniveau', 
+        'Museum waar een kleine selectie van de vondsten uit <b>Dion</b> tentoon worden gesteld.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Busreis', 
+        40.35491675, 22.26242065, 
+        '741 meter boven zeeniveau', 
+        'Onderweg van <b>Dion</b> naar <b>Veroia</b>.',
+        'icons/bus_left.png'
+    ],
+    [
+        'Archeologisch Museum van Veroia', 
+        40.5291175, 22.2065529, 
+        '132 meter boven zeeniveau', 
+        'Museum met regionale vondsten.',
+        'icons/icon_IV.png'
+    ],
+    [
+        'Busreis', 
+        40.59023175, 22.16903687, 
+        '87 meter boven zeeniveau', 
+        'Onderweg van <b>Veroia</b> naar <b>Naoussa</b>.',
+        'icons/bus_left.png'
+    ],
+    [
+        'Overnachting', 
+        40.630225, 22.07229495, 
+        '339 meter boven zeeniveau', 
+        'Overnachting in <b>Naoussa</b>.',
+        'icons/hotel.png'
+    ],
 ];
 
 var map;
